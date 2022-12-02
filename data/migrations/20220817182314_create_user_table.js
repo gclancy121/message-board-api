714b6eae064ceb9fe20de332272039ba9fd76a44
Gage Clancy <gclancy121@gmail.com>
2022-12-02 12:49:16 -0600

    added capability to store security questions

--- data/migrations/20220817182314_create_user_table.js
+++ data/migrations/20220817182314_create_user_table.js
@@ -10,6 +10,8 @@ exports.up = function(knex) {
     user.string('about_me', 3000).notNullable().defaultTo("I'm a new weeblet and haven't changed this yet.");
     user.string('profile_picture').notNullable().defaultTo('https://i.pinimg.com/736x/ba/a2/4e/baa24e0fbad55140f9103dcfc44894b0.jpg');
     user.string('fav_waifu').notNullable().defaultTo('Yuuki Asuna');
+    user.string('security_question').notNullable();
+    user.string('security_question_answer').notNullable();
   })
 };
 
